5677ab88f3c0b9ab08e5ef2fabc0b36710a6bf32
Robert Smith <rob@bettertogethersolutions.com>
2025-01-16 15:43:19 -0330

    Rubocop fixes

--- db/migrate/20241025145031_remove_messages_content_column_null.better_together.rb
+++ db/migrate/20241025145031_remove_messages_content_column_null.better_together.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This migration comes from better_together (originally 20241025144852)
 class RemoveMessagesContentColumnNull < ActiveRecord::Migration[7.1]
   def change
