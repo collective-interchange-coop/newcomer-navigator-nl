f10a4d77e5fd78dd679bb283812c3a2da903f7df
Robert Smith <rob@bettertogethersolutions.com>
2025-04-18 13:05:42 -0230

    Replace class constants with class attributes for overridable class values

--- app/models/concerns/new_to_nl_navigation_item.rb
+++ app/models/concerns/new_to_nl_navigation_item.rb
@@ -4,11 +4,9 @@ module NewToNlNavigationItem # rubocop:todo Style/Documentation
   extend ::ActiveSupport::Concern
 
   included do
-    new_routes = {
+    self.route_names.merge({
       partners: 'partners_path',
       resources: 'resources_path'
-    }
-
-    self::ROUTE_NAMES = self::ROUTE_NAMES.merge(new_routes).freeze
+    })
   end
 end
