0fd84c9dbe064b8dd69886d087e14a8978f67c2d
Robert Smith <rob@bettertogethersolutions.com>
2025-08-04 22:54:54 -0230

    Stub partner data in map specs

--- spec/models/partner_collection_map_spec.rb
+++ spec/models/partner_collection_map_spec.rb
@@ -3,15 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe PartnerCollectionMap, type: :model do
-  def connect_partner_with_space(space: nil, latitude: nil, longitude: nil)
-    space ||= create(:better_together_geography_space, latitude: latitude, longitude: longitude)
-    building = create(:better_together_infrastructure_building)
-    building.create_geospatial_space(space: space)
-    partner = create(:partner)
-    BetterTogether::Infrastructure::BuildingConnection.create!(building: building, connection: partner, position: 1)
-    [partner, space]
-  end
-
   describe '.records' do
     let!(:older_partner) { create(:partner, created_at: 2.days.ago) }
     let!(:newer_partner) { create(:partner, created_at: 1.day.ago) }
@@ -39,25 +30,35 @@ RSpec.describe PartnerCollectionMap, type: :model do
   end
 
   describe '#leaflet_points' do
-    it 'returns unique flattened leaflet points from records', :aggregate_failures do
-      partner1, = connect_partner_with_space(latitude: 1.0, longitude: 2.0)
-      partner2, = connect_partner_with_space(latitude: 3.0, longitude: 4.0)
+    let(:first_partner) { create(:partner) }
+    let(:second_partner) { create(:partner) }
 
-      allow(described_class).to receive(:records).and_return([partner1, partner2])
+    before do
+      allow(first_partner).to receive(:leaflet_points).and_return([{ lat: 1.0, lng: 2.0 }])
+      allow(second_partner).to receive(:leaflet_points).and_return([{ lat: 3.0, lng: 4.0 }])
+      allow(described_class).to receive(:records).and_return([first_partner, second_partner])
+    end
 
+    it 'returns unique flattened leaflet points from records' do
       coords = described_class.new.leaflet_points.map { |p| [p[:lat], p[:lng]] }
       expect(coords).to contain_exactly([1.0, 2.0], [3.0, 4.0])
     end
   end
 
   describe '#spaces' do
-    it 'returns unique flattened spaces from records', :aggregate_failures do
-      partner1, space1 = connect_partner_with_space
-      partner2, space2 = connect_partner_with_space
+    let(:first_space) { create(:better_together_geography_space) }
+    let(:second_space) { create(:better_together_geography_space) }
+    let(:first_partner) { create(:partner) }
+    let(:second_partner) { create(:partner) }
 
-      allow(described_class).to receive(:records).and_return([partner1, partner2])
+    before do
+      allow(first_partner).to receive(:spaces).and_return([first_space])
+      allow(second_partner).to receive(:spaces).and_return([second_space])
+      allow(described_class).to receive(:records).and_return([first_partner, second_partner])
+    end
 
-      expect(described_class.new.spaces).to contain_exactly(space1, space2)
+    it 'returns unique flattened spaces from records' do
+      expect(described_class.new.spaces).to contain_exactly(first_space, second_space)
     end
   end
 end
--- spec/system/partners_map_spec.rb
+++ spec/system/partners_map_spec.rb
@@ -5,20 +5,15 @@ require 'rails_helper'
 RSpec.describe 'Partners map', type: :system do
   before do
     driven_by(:rack_test)
-
-    partner = create(:partner)
-    building = create(:better_together_infrastructure_building)
-    building.create_geospatial_space(space: create(:better_together_geography_space, latitude: 1.0, longitude: 2.0))
-    BetterTogether::Infrastructure::BuildingConnection.create!(
-      building: building,
-      connection: partner,
-      position: 1,
-      primary_flag: true
-    )
   end
 
   it 'renders the leaflet map on the index page' do
+    partner = create(:partner)
+    allow(partner).to receive(:leaflet_points).and_return([{ lat: 1.0, lng: 2.0 }])
+    allow(PartnerCollectionMap).to receive(:records).and_return([partner])
+
     visit partners_path(locale: I18n.locale)
+
     expect(page).to have_css('.partners-map .map[data-controller="better_together--map"]')
   end
 end
