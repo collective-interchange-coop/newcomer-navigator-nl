fabbf46478434c80729fbe79c8008c24acab7d88
Robert Smith <rob@bettertogethersolutions.com>
2025-11-07 12:09:34 -0330

    Refactor caching and render logic in ContentHelper to improve readability

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -6,9 +6,15 @@ module ContentHelper # rubocop:todo Style/Documentation
 
     return unless disclaimer
 
-    # cache disclaimer.cache_key_with_version do
-    content_tag :small, disclaimer.content, class: 'fst-italic', style: 'font-size: 0.6em;'
-    # end
+    content = content_tag :small, disclaimer.content, class: 'fst-italic', style: 'font-size: 0.6em;'
+
+    if controller.perform_caching
+      cache disclaimer.cache_key_with_version do
+        content
+      end
+    else
+      content
+    end
   end
 
   def render_funder_content
@@ -16,10 +22,9 @@ module ContentHelper # rubocop:todo Style/Documentation
 
     return unless funder
 
-    # cache funder.cache_key_with_version, expires_in: 1.day do
     content_tag :div, class: 'container content my-3', id: 'new-to-nl-funder-message' do
       render partial: 'better_together/content/blocks/template', locals: { template: funder }
     end
-    # end
+  end
   end
 end
